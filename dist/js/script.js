a6a705c6febf578f08c0e706040958bc111a820a
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-29 13:40:46 -0500

    21.12-dev-6

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,7 +24,7 @@ API.Plugins.files = {
 							html += '<i class="fas fa-'+defaults.icon+' bg-'+defaults.color+'"></i>';
 							html += '<div class="timeline-item">';
 								html += '<span class="time"><i class="fas fa-clock mr-2"></i><time class="timeago" datetime="'+dataset.created.replace(/ /g, "T")+'">'+dataset.created+'</time></span>';
-								html += '<h3 class="timeline-header border-0">'+dataset.filename+'('+API.Helper.getFileSize(dataset.size)+') was uploaded</h3>';
+								html += '<h3 class="timeline-header border-0">'+dataset.filename+' ('+API.Helper.getFileSize(dataset.size)+') was uploaded</h3>';
 							html += '</div>';
 						html += '</div>';
 						layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').after(html);
@@ -37,12 +37,134 @@ API.Plugins.files = {
 						layout.timeline.append(items);
 						element.find('i').first().addClass('pointer');
 						element.find('i').first().off().click(function(){
-							console.log('Download')
+							console.log('Download');
 						});
 						if(callback != null){ callback(element); }
 					}
 				}, 100);
 			}
+		},
+	},
+	Layouts:{
+		details:{
+			tab:function(data,layout,options = {},callback = null){
+				if(options instanceof Function){ callback = options; options = {}; }
+				var defaults = {};
+				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				API.GUI.Layouts.details.tab(data,layout,{icon:"fas fa-file",text:API.Contents.Language["Files"]},function(data,layout,tab,content){
+					API.Builder.Timeline.add.filter(layout,'files','Files');
+					layout.content.files = content;
+					layout.tabs.files = tab;
+					var html = '<div class="row p-3">';
+						html += '<div class="col-md-12">';
+							html += '<div class="input-group">';
+								if(API.Auth.validate('plugin', 'files', 2)){
+									html += '<div class="btn-group mr-3">';
+										html += '<button data-action="create" class="btn btn-success"><i class="fas fa-file-upload" aria-hidden="true"></i></button>';
+									html += '</div>';
+								}
+								html += '<input type="text" class="form-control">';
+								html += '<div class="input-group-append pointer" data-action="clear"><span class="input-group-text"><i class="fas fa-times"></i></span></div>';
+								html += '<div class="input-group-append"><span class="input-group-text"><i class="icon icon-search mr-1"></i>'+API.Contents.Language['Search']+'</span></div>';
+							html += '</div>';
+						html += '</div>';
+					html += '</div>';
+					html += '<div class="row px-2 py-0">';
+						html += '<table class="table table-sm table-striped table-hover mb-0">';
+							html += '<thead>';
+								html += '<tr>';
+									html += '<th data-header="filename">'+API.Contents.Language['Filename']+'</th>';
+									html += '<th data-header="size">'+API.Contents.Language['Size']+'</th>';
+									html += '<th data-header="meta">'+API.Contents.Language['Meta']+'</th>';
+									html += '<th data-header="action">'+API.Contents.Language['Action']+'</th>';
+								html += '</tr>';
+							html += '</thead>';
+							html += '<tbody></tbody>';
+						html += '</table>';
+					html += '</div>';
+					content.append(html);
+					if(API.Helper.isSet(data,['relations','files'])){
+						for(var [id, file] of Object.entries(data.relations.files)){
+							API.Plugins.files.Layouts.GUI.addRow(file,layout);
+						}
+					}
+				});
+				// API.Plugins.files.Layouts.details.Events(data,layout);
+				if(callback != null){ callback(dataset,layout); }
+			},
+			GUI:{
+				addRow:function(dataset,layout,options = {},callback = null){
+					var url = new URL(window.location.href);
+					if(options instanceof Function){ callback = options; options = {}; }
+					var defaults = {};
+					for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+					var body = layout.content.files.find('tbody');
+					var meta = JSON.parse(dataset.meta);
+					var html = '<tr data-csv="'+csv+'" data-id="'+dataset.id+'">';
+						html += '<td class="pointer">'+dataset.filename+'</td>';
+						html += '<td class="pointer">'+API.Helper.getFileSize(dataset.size)+'</td>';
+						html += '<td class="pointer"></td>';
+						html += '<td>';
+							html += '<div class="btn-group btn-block m-0">';
+								html += '<button class="btn btn-xs btn-warning" data-action="download"><i class="fas fa-file-download mr-1"></i>'+API.Contents.Language['Download']+'</button>';
+							html += '</div>';
+						html += '</td>';
+					html += '</tr>';
+					body.append(html);
+					var tr = body.find('tr').last();
+					if(callback != null){ callback(dataset,layout,tr); }
+				},
+			},
+			Events:function(dataset,layout,options = {},callback = null){
+				var url = new URL(window.location.href);
+				if(options instanceof Function){ callback = options; options = {}; }
+				var defaults = {field: "name"};
+				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				if(API.Auth.validate('plugin', 'files', 2)){
+					layout.content.files.find('button').off().click(function(){
+					  if(!layout.content.files.find('textarea').summernote('isEmpty')){
+					    var note = {
+					      by:API.Contents.Auth.User.id,
+					      content:layout.content.files.find('textarea').summernote('code'),
+					      relationship:url.searchParams.get("p"),
+					      link_to:dataset.this.dom.id,
+					      status:dataset.this.raw.status,
+					    };
+					    layout.content.files.find('textarea').val('');
+					    layout.content.files.find('textarea').summernote('code','');
+					    layout.content.files.find('textarea').summernote('destroy');
+					    layout.content.files.find('textarea').summernote({
+					      toolbar: [
+					        ['font', ['fontname', 'fontsize']],
+					        ['style', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+					        ['color', ['color']],
+					        ['paragraph', ['style', 'ul', 'ol', 'paragraph', 'height']],
+					      ],
+					      height: 250,
+					    });
+					    API.request(url.searchParams.get("p"),'file',{data:note},function(result){
+					      var data = JSON.parse(result);
+					      if(data.success != undefined){
+									API.Plugins.files.Timeline.object(data.output.note.dom,layout);
+					      }
+					    });
+					    layout.tabs.find('a').first().tab('show');
+					  } else {
+					    layout.content.files.find('textarea').summernote('destroy');
+					    layout.content.files.find('textarea').summernote({
+					      toolbar: [
+					        ['font', ['fontname', 'fontsize']],
+					        ['style', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+					        ['color', ['color']],
+					        ['paragraph', ['style', 'ul', 'ol', 'paragraph', 'height']],
+					      ],
+					      height: 250,
+					    });
+					    alert(API.Contents.Language['File is empty']);
+					  }
+					});
+				}
+			},
 		},
 	},
 }
