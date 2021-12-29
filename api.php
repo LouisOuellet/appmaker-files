0366806e649e619e39582022c9cd3da7499196ac
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-29 14:48:41 -0500

    21.12-dev-15

--- api.php
+++ api.php
@@ -1,2 +1,42 @@
 <?php
-class filesAPI extends CRUDAPI {}
+class filesAPI extends extendedAPI {
+  public function save($file){
+    if(!isset($this->Settings['plugins']['files']['settings']['blacklist'])||(isset($this->Settings['plugins']['files']['settings']['blacklist']) && is_array($this->Settings['plugins']['files']['settings']['blacklist']) && !in_array($file['type'], $this->Settings['plugins']['files']['settings']['blacklist']))){
+      $md5 = md5($file["file"]);
+      $files = $this->Auth->query('SELECT * FROM `files` WHERE `checksum` = ?',$md5)->fetchAll()->all();
+      if(empty($files)){
+        $query = $this->Auth->query('INSERT INTO `files` (
+          `created`,
+          `modified`,
+          `owner`,
+          `updated_by`,
+          `name`,
+          `filename`,
+          `checksum`,
+          `file`,
+          `type`,
+          `size`,
+          `encoding`,
+          `meta`,
+          `isAttachment`
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
+          date("Y-m-d H:i:s"),
+          date("Y-m-d H:i:s"),
+          $this->Auth->User['id'],
+          $this->Auth->User['id'],
+          $file["name"],
+          $file["filename"],
+          $md5,
+          $file["file"],
+          $file["type"],
+          $file["size"],
+          $file["encoding"],
+          $file["meta"],
+          $file["isAttachment"]
+        );
+        set_time_limit(20);
+        return $query->dump()['insert_id'];
+      }
+    }
+  }
+}
