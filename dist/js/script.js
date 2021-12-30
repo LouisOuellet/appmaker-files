02cc9e1e29bd156c1478cab105abe0fed5b8f8ab
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-30 15:06:16 -0500

    21.12-dev-58

--- dist/js/script.js
+++ dist/js/script.js
@@ -64,7 +64,7 @@ API.Plugins.files = {
 							}, 100);
 							break;
 						default:
-							console.log(action,zone,data);
+							// console.log(action,zone,data);
 							break;
 					}
 				});
