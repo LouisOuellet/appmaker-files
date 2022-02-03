2d372276e313ad4799cdc1b82d1a478cb3692537
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-03 14:12:15 -0500

    22.02-dev-117

--- helper.php
+++ helper.php
@@ -2,12 +2,12 @@
 class filesHelper extends Helper {
 
 	public function buildRelation($relations){
-    // if(isset($relations['files'])){
-		// 	foreach($relations['files'] as $id => $file){
-		// 		unset($relations['files'][$id]['file']);
-		// 	}
-    // }
-    // return $relations;
+    if(isset($relations['files'])){
+			foreach($relations['files'] as $id => $file){
+				unset($relations['files'][$id]['file']);
+			}
+    }
+    return $relations;
   }
 	public function convertToDOM($result){
 		if((!empty($result))&&(is_array($result))){
